7657ca6cce0d2942b31c318d381b17dd0cba5799
russellwenz <rwenz@ventera.com>
2024-09-09 10:36:30 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "https://www.google.com/maps/d/edit?mid=1m8GPcoA1aAwH21s4DwhhcVd13f-rTeSv&usp=sharing" --output data.kml
+curl "https://www.google.com/maps/d/edit?mid=1m8GPcoA1aAwH21s4DwhhcVd13f-rTeSv&usp=sharing&forcekml=1" --output data.kml
